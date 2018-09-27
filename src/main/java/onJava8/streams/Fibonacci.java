939175118e9f2a02aca606659ea4339387c9566d
Jerry Guo <jerry.guo@internal.finpacific.com>
2018-09-28 09:29:37 +1000

    stream

---
+++ src/main/java/onJava8/streams/Fibonacci.java
@@ -0,0 +1,20 @@
+package onJava8.streams;
+
+import java.util.stream.Stream;
+
+public class Fibonacci {
+	
+	int x = 1;
+	Stream<Integer> numbers(){
+		return Stream.iterate(0, i->{
+			int result = x + i ;
+			x = i ;
+			return result;
+		});
+	}
+	public static void main(String[] args) {
+		new Fibonacci().numbers()
+		    .limit(4).forEach(System.out::println);
+	}
+
+}

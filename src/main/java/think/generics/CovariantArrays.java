a77f25864f5ec20e280d8cf50109f5b2f9021fc3
Jerry <jerryguowei@gmail.com>
2018-10-24 10:34:12 +1100

    generics

---
+++ src/main/java/think/generics/CovariantArrays.java
@@ -0,0 +1,23 @@
+package think.generics;
+
+class Fruit {}
+class Apple extends Fruit {}
+class Jonathan extends Apple {}
+class Orange extends Fruit {}
+public class CovariantArrays {
+  public static void main(String[] args) {
+	  Fruit[] fruit = new Apple[10];
+	  fruit[0] = new Apple();
+	  fruit[1] = new Jonathan();
+	  try {
+		  fruit[0]= new Fruit();
+	  }catch (Exception e) {
+         e.printStackTrace();
+	  }
+	  try {
+		  fruit[0] = new Orange();
+	  }catch (Exception e) {
+           e.printStackTrace();
+	  }
+  }
+}

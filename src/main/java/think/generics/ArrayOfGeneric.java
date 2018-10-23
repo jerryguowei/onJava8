aec98984885f15e49552a091abf61d3173fae7ef
Jerry <jerryguowei@gmail.com>
2018-10-23 11:30:07 +1100

    generics array

---
+++ src/main/java/think/generics/ArrayOfGeneric.java
@@ -0,0 +1,13 @@
+package think.generics;
+
+public class ArrayOfGeneric {
+   static final int SIZE = 100;
+   static Generic<Integer>[] gia;
+   @SuppressWarnings("unchecked")
+   public static void main(String[] args) {
+	   //gia = (Generic<Integer>[]) new Object[SIZE];  //Runtime exception ClassCastException
+	   gia = (Generic<Integer>[]) new Generic[SIZE];
+	   System.out.println(gia.getClass().getSimpleName());
+	   gia[0] = new Generic<Integer>();
+   }
+}
---
+++ src/main/java/think/generics/ArrayOfGenericReference.java
@@ -0,0 +1,6 @@
+package think.generics;
+
+class Generic<T> {}
+public class ArrayOfGenericReference {
+ static Generic<Integer>[] gia;
+}
